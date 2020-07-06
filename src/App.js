d8a3a7034330b0f7dd22d9b2e43a1a7a6994b916
William R <wxavier8@gmail.com>
2020-07-05 23:43:15 -0300

    menu-item, directory, homepageç

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './components/pages/homepage/homepage.component';
 
 
 function App() {
