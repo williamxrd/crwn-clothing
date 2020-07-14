c9d4f9739f680eeecd573fdc8b89e6dfd225f732
William R <wxavier8@gmail.com>
2020-07-14 17:19:56 -0300

    header sign in v0.1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component.jsx';
+import SignInAndSignUp from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>      
     </div>
   );
